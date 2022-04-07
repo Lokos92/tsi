d74f7f04008e28939480307f7b6f0f1f79d01018
Lokos92 <101327079+Lokos92@users.noreply.github.com>
2022-04-07 21:15:22 +0300

    Add files via upload

---
+++ aleks/src/main/java/com/tsi/aleks/AleksApplication.java
@@ -0,0 +1,13 @@
+package com.tsi.aleks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AleksApplication.class, args);
+	}
+
+}
---
+++ aleks/src/test/java/com/tsi/aleks/AleksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsi.aleks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
